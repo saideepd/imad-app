8c3f67abe552bb2c08e10c42e5ae25f492295675
Saideep Dicholkar <30663492+saideepd@users.noreply.github.com>
2017-08-23 22:55:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ element.innerHTML = "New Value";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function () {
-    var internal = setInterval(moveRight, 100);
+    var internal = setInterval(moveRight, 50);
 }
