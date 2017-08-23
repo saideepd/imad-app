25cc9db2d75d24f4def140f70b08025a499215c3
Saideep Dicholkar <30663492+saideepd@users.noreply.github.com>
2017-08-23 23:12:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var counter = 0;
+app.get('/counter', funcion (req,res)) {
+    counter = counter + 1;
+    res.send(counter.toString());
+}
 app.get("/:articleName", function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} constant object for article one
