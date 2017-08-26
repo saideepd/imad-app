c305f1fe3ca17a6a75a1a5918da6778edbd359b0
Saideep Dicholkar <30663492+saideepd@users.noreply.github.com>
2017-08-26 23:47:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/counter', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function (req, res) { // URL: /submit-name?name=xxxx
+app.get('/submit-name', function (req, res) { // /submit-name?name=xxxx
    // Get the name from the request
    var name = req.query.name;
    
