fc76f5a9d34986b84852253b69c64b7fc2b45d7a
Saideep Dicholkar <30663492+saideepd@users.noreply.github.com>
2017-08-27 14:46:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'dicholkarsaideep',
+    database: 'dicholkarsaideep',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -86,6 +95,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+   // Make a select request
+   // Return a response with the results
+   pool.query('SLECT * FROM test', function (err, result) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+   });
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
