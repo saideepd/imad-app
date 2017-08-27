c29017ce81cf233dba643c202ec0fdee4c079340
Saideep Dicholkar <30663492+saideepd@users.noreply.github.com>
2017-08-27 16:38:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,8 +128,8 @@ app.get("/articles/:articleName", function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} constant object for article one
     
-    //SELECT * FROM article WHERE title = ''; DELETE WHERE a='asdf'
-    pool.query("SELECT * FROM article WHERE title = '" + res.params.articleName + "'", function (err, result) {
+    //SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
        if(err) {
            res.status(500).send(err.toString());
        } else {
